0142ab5ef55dd8672b43fdb7c35400e24bcdb13a
Blagovest-Todorov <blagovest.stod@abv.bg>
2023-02-19 13:49:43 +0200

    Add project files.

---
+++ Summator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Summator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal result = Summator.Sum(new decimal[] {1, 2, 3, 4, 5, 6 });
+
+            if (result == 21)
+            {
+                Console.WriteLine("TestPass");
+            }            
+        }
+    }
+}
---
+++ Summator/Summator.cs
@@ -0,0 +1,17 @@
+﻿namespace Summator
+{
+    public static class Summator
+    {
+        public static decimal Sum(params decimal[]arr) 
+        {
+            decimal sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+    }
+}
